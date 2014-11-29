013622353986a69d0d9853148d37209d10f35904
tair_mustafaiev@epam.com <The>
2014-11-30 01:27:57 +0200

    removed test, fixed facade

--- jobizer-plugin/src/main/java/com/epam/jobizer/jobdsl/JobsDslFacade.java
+++ jobizer-plugin/src/main/java/com/epam/jobizer/jobdsl/JobsDslFacade.java
@@ -1,8 +1,53 @@
 package com.epam.jobizer.jobdsl;
 
+import com.google.common.collect.Sets;
+import hudson.EnvVars;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import javaposse.jobdsl.dsl.DslScriptLoader;
+import javaposse.jobdsl.dsl.GeneratedJob;
+import javaposse.jobdsl.dsl.JobManagement;
+import javaposse.jobdsl.dsl.ScriptRequest;
+import javaposse.jobdsl.plugin.JenkinsJobManagement;
+import jenkins.model.Jenkins;
+import lombok.extern.java.Log;
+
 import java.io.IOException;
+import java.net.URI;
 import java.util.Set;
 
-public interface JobsDslFacade {
-    Set<String> run(String[] fileNames) throws IOException, InterruptedException;
+@Log
+public class JobsDslFacade {
+
+    private final BuildListener buildListener;
+    private final AbstractBuild build;
+    private final URI workspaceURI;
+
+    public JobsDslFacade(final AbstractBuild build, final BuildListener listener, final URI uri) throws IOException,
+            InterruptedException {
+        this.build = build;
+        this.buildListener = listener;
+        this.workspaceURI = uri;
+    }
+
+    public Set<String> run(final String[] fileNames) throws IOException, InterruptedException {
+
+        EnvVars env = build.getEnvironment(buildListener);
+        env.putAll(build.getBuildVariables());
+
+        final JobManagement jm = new JenkinsJobManagement(buildListener.getLogger(), env, build);
+
+        Set<String> jobs = Sets.newHashSet();
+        for (String fileName : fileNames) {
+            ScriptRequest request = new ScriptRequest(fileName, null, this.workspaceURI.toURL(), false);
+            Set<GeneratedJob> generatedItems = DslScriptLoader.runDslEngine(request, jm);
+            for (GeneratedJob job : generatedItems) {
+                log.info("Job with name " + job.getJobName() + " created");
+            }
+
+        }
+        Jenkins.getInstance().rebuildDependencyGraph();
+
+        return jobs;
+    }
 }
--- jobizer-plugin/src/main/java/com/epam/jobizer/plugin/JobConfigurationBuilder.java
+++ jobizer-plugin/src/main/java/com/epam/jobizer/plugin/JobConfigurationBuilder.java
@@ -1,21 +1,20 @@
 package com.epam.jobizer.plugin;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-
-import org.kohsuke.stapler.DataBoundConstructor;
+import com.epam.jobizer.flow.FlowExecutionException;
+import com.epam.jobizer.flow.FlowExecutor;
+import com.epam.jobizer.jobdsl.JobsDslFacade;
+import com.epam.jobizer.plugin.dsl.PipelineDsl;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
 import hudson.tasks.Builder;
 import lombok.extern.java.Log;
+import org.kohsuke.stapler.DataBoundConstructor;
 
-import com.epam.jobizer.flow.FlowExecutionException;
-import com.epam.jobizer.flow.FlowExecutor;
-import com.epam.jobizer.jobdsl.JobsDslFacade;
-import com.epam.jobizer.jobdsl.impl.JobsDslFacadeImpl;
-import com.epam.jobizer.plugin.dsl.PipelineDsl;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.logging.Level;
 
 @Log
 public class JobConfigurationBuilder extends Builder {
@@ -26,12 +25,13 @@ public class JobConfigurationBuilder extends Builder {
 
     @Override
     public boolean perform(final AbstractBuild build, final Launcher launcher, final BuildListener listener) throws IOException, InterruptedException {
-        String workspacePath = build.getWorkspace().toURI().getPath();
+        URI uri = build.getWorkspace().toURI();
+        String workspacePath = uri.getPath();
         File pipeLine = new File(workspacePath + "/.pipeline");
         boolean result = false;
         if (pipeLine.exists()) {
             log.info("Is about to start pipeline");
-            JobsDslFacade jobFacade = new JobsDslFacadeImpl(build, listener);
+            JobsDslFacade jobFacade = new JobsDslFacade(build, listener, uri);
             try {
                 new PipelineDsl(jobFacade, new FlowExecutor()).execute(pipeLine);
                 result = true;
