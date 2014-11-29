8b9f48b60a1abf00cbffb7e072eaa5a158afdebf
Izzet Mustsfaiev <webdizz@gmail.com>
2014-11-29 15:52:25 +0200

    Remove unnecessary annotation due to failure during plugin start

--- jobizer-plugin/src/main/java/com/epam/jobizer/plugin/JobConfigurationBuilder.java
+++ jobizer-plugin/src/main/java/com/epam/jobizer/plugin/JobConfigurationBuilder.java
@@ -1,8 +1,21 @@
 package com.epam.jobizer.plugin;
 
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
 import hudson.tasks.Builder;
 import lombok.extern.java.Log;
 
 @Log
 public class JobConfigurationBuilder extends Builder {
+    @Override
+    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
+        log.info("Hello from Jenkins");
+        // This is where you 'build' the project.
+        // Since this is a dummy, we just say 'hello world' and call that a build.
+
+        // This also shows how you can consult the global configuration of the builder
+        return true;
+    }
+
 }
