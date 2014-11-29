4d1d6f1ec5f1383a374559a138fbd7dac45e3f8b
Izzet Mustsfaiev <webdizz@gmail.com>
2014-11-29 16:03:11 +0200

    Added default constructor for builder

--- jobizer-plugin/src/main/java/com/epam/jobizer/plugin/JobConfigurationBuilder.java
+++ jobizer-plugin/src/main/java/com/epam/jobizer/plugin/JobConfigurationBuilder.java
@@ -1,5 +1,6 @@
 package com.epam.jobizer.plugin;
 
+import org.kohsuke.stapler.DataBoundConstructor;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
@@ -8,8 +9,13 @@ import lombok.extern.java.Log;
 
 @Log
 public class JobConfigurationBuilder extends Builder {
+
+    @DataBoundConstructor
+    public JobConfigurationBuilder() {
+    }
+
     @Override
-    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
+    public boolean perform(final AbstractBuild build, final Launcher launcher, final BuildListener listener) {
         log.info("Hello from Jenkins");
         // This is where you 'build' the project.
         // Since this is a dummy, we just say 'hello world' and call that a build.
