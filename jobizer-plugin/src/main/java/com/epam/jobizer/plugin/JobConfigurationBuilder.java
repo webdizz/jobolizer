0b27c88d93e26a05dbdd20c4a7c75d98c5fcc1b4
Izzet Mustsfaiev <webdizz@gmail.com>
2014-11-29 14:44:36 +0200

    Empty plugin implementation

---
+++ jobizer-plugin/src/main/java/com/epam/jobizer/plugin/JobConfigurationBuilder.java
@@ -0,0 +1,8 @@
+package com.epam.jobizer.plugin;
+
+import hudson.tasks.Builder;
+import lombok.extern.java.Log;
+
+@Log
+public class JobConfigurationBuilder extends Builder {
+}
---
+++ jobizer-plugin/src/main/java/com/epam/jobizer/plugin/PluginDescriptor.java
@@ -0,0 +1,20 @@
+package com.epam.jobizer.plugin;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.tasks.Builder;
+import jenkins.YesNoMaybe;
+import lombok.extern.java.Log;
+
+@Extension(dynamicLoadable = YesNoMaybe.YES)
+@Log
+public class PluginDescriptor extends Descriptor<Builder> {
+
+    public PluginDescriptor() {
+        super(JobConfigurationBuilder.class);
+    }
+
+    public String getDisplayName() {
+        return "Treat build as code";
+    }
+}
