359dea7ace386434520bea988656a8bcf56f8eba
Mustafaiev Zinur <zinur@mustafaiev.com>
2014-11-30 02:58:15 +0200

    [zm] real  fix

--- jobizer-plugin/src/main/java/com/epam/jenkins/plugin/jobolizer/flow/FlowExecutable.java
+++ jobizer-plugin/src/main/java/com/epam/jenkins/plugin/jobolizer/flow/FlowExecutable.java
@@ -1,6 +1,8 @@
 package com.epam.jenkins.plugin.jobolizer.flow;
 
+import java.io.IOException;
+
 public interface FlowExecutable {
 
-    void run(String buildFlowPath) throws FlowExecutionException;
+    void run(String buildFlowPath) throws FlowExecutionException, IOException, InterruptedException;
 }
--- jobizer-plugin/src/main/java/com/epam/jenkins/plugin/jobolizer/flow/FlowExecutor.java
+++ jobizer-plugin/src/main/java/com/epam/jenkins/plugin/jobolizer/flow/FlowExecutor.java
@@ -1,26 +1,32 @@
 package com.epam.jenkins.plugin.jobolizer.flow;
 
+import com.cloudbees.plugins.flow.BuildFlow;
 import com.cloudbees.plugins.flow.FlowDSL;
 import com.cloudbees.plugins.flow.FlowRun;
+import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
 import lombok.extern.java.Log;
+
+import java.io.IOException;
 
 @Log
 
 public class FlowExecutor implements FlowExecutable {
 
-    private FlowRun build;
+    private AbstractBuild build;
 
     private BuildListener listener;
 
-    public FlowExecutor (final FlowRun build, final BuildListener listener) {
+    public FlowExecutor (final AbstractBuild build, final BuildListener listener) {
         this.build = build;
         this.listener = listener;
     }
 
     @Override
-    public void run (final String buildFlowPath) throws FlowExecutionException {
-
-        new FlowDSL().executeFlowScript(build, buildFlowPath, listener);
+    public void run (final String buildFlowPath) throws FlowExecutionException, IOException, InterruptedException {
+        BuildFlow buildFlow = new BuildFlow(build.getParent().getParent(), "job");
+        FlowRun flowRun = new FlowRun(buildFlow);
+        String fileContent = build.getWorkspace().child(buildFlowPath).readToString();
+        new FlowDSL().executeFlowScript(flowRun, fileContent, listener);
     }
 }
