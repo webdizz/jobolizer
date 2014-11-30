83e96ccd4864a0d7653c7c8b6f4c6439098f483b
Izzet Mustsfaiev <webdizz@gmail.com>
2014-11-30 02:45:58 +0200

    Improve code readability

--- jobizer-plugin/src/main/java/com/epam/jenkins/plugin/jobolizer/JobExecutionConfigurationBuilder.java
+++ jobizer-plugin/src/main/java/com/epam/jenkins/plugin/jobolizer/JobExecutionConfigurationBuilder.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.logging.Level;
 
-import com.cloudbees.plugins.flow.BuildFlow;
-import com.cloudbees.plugins.flow.FlowRun;
 import org.kohsuke.stapler.DataBoundConstructor;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
@@ -14,12 +12,15 @@ import hudson.model.BuildListener;
 import hudson.tasks.Builder;
 import lombok.extern.java.Log;
 
+import com.cloudbees.plugins.flow.BuildFlow;
+import com.cloudbees.plugins.flow.FlowRun;
+import com.epam.jenkins.plugin.jobolizer.dsl.Pipeline;
+import com.epam.jenkins.plugin.jobolizer.flow.FlowExecutable;
 import com.epam.jenkins.plugin.jobolizer.flow.FlowExecutionException;
 import com.epam.jenkins.plugin.jobolizer.flow.FlowExecutor;
 import com.epam.jenkins.plugin.jobolizer.job.JobCreatable;
 import com.epam.jenkins.plugin.jobolizer.job.JobCreationException;
 import com.epam.jenkins.plugin.jobolizer.job.JobCreator;
-import com.epam.jenkins.plugin.jobolizer.dsl.Pipeline;
 
 @Log
 public class JobExecutionConfigurationBuilder extends Builder {
@@ -36,11 +37,10 @@ public class JobExecutionConfigurationBuilder extends Builder {
         boolean result = false;
         if (pipeLine.exists()) {
             log.info("Is about to start pipeline");
-            JobCreatable jobFacade = new JobCreator(build, listener, uri);
             try {
-                BuildFlow buildFlow = new BuildFlow(build.getParent().getParent(), "test");
-                FlowRun flowRun = new FlowRun(buildFlow);
-                new Pipeline(jobFacade, new FlowExecutor(flowRun,listener )).execute(pipeLine);
+                JobCreatable jobCreator = createJobCreatable(build, listener, uri);
+                FlowExecutable flowExecutor = createFlowExecutable(build, listener);
+                execute(pipeLine, jobCreator, flowExecutor);
                 result = true;
             } catch (FlowExecutionException fee) {
                 log.log(Level.SEVERE, "Unable to perform execution of build flow", fee);
@@ -51,6 +51,20 @@ public class JobExecutionConfigurationBuilder extends Builder {
             log.warning("Pipeline file does not exist in directory: " + workspacePath);
         }
         return result;
+    }
+
+    private void execute(final File pipeLine, final JobCreatable jobCreator, final FlowExecutable flowExecutor) {
+        new Pipeline(jobCreator, flowExecutor).execute(pipeLine);
+    }
+
+    private JobCreatable createJobCreatable(final AbstractBuild build, final BuildListener listener, final URI uri) {
+        return new JobCreator(build, listener, uri);
+    }
+
+    private FlowExecutable createFlowExecutable(final AbstractBuild build, final BuildListener listener) throws IOException {
+        BuildFlow buildFlow = new BuildFlow(build.getParent().getParent(), "test");
+        FlowRun flowRun = new FlowRun(buildFlow);
+        return new FlowExecutor(flowRun, listener);
     }
 
 }
